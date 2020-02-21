20e91ea5cead8a19295efe89d5e520b9d0f58712
Owen Adrian <akrivus@gmail.com>
2020-02-20 19:28:12 -0700

    Renamed to PhotoGo.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Sgallery
+module PhotoGo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
