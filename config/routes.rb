3e0256a22afa6a68b887b700b5ace08672a39c49
Owen Adrian <akrivus@gmail.com>
2020-02-21 21:49:48 -0700

    Removing Robust (too extensive; wait until Sunday.)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :photos, :comments, :albums
-  resources :users do
-    
-  end
-
+  resources :photos, :comments, :albums, :users
+  post '/users/authenticate', to: 'users#authenticate'
+  get '/users/login', to: 'users#login'
   get '/', to: 'index#index'
 end
