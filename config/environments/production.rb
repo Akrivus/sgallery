2a67aa7f4c363a078eba410db840f1ae121e6002
Owen Adrian <akrivus@gmail.com>
2020-02-24 14:49:21 -0700

    Updating e-mail credentials.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'gmail.com' }
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
