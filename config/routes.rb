858a103a930a2cdd5fac88bc4aaff26d333fd616
Owen Adrian <akrivus@gmail.com>
2020-02-24 11:16:39 -0700

    Drawing nested routes for comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments, :albums, :photos
   resources :users do
     post 'login', on: :collection
+  end
+  resources :albums
+  resources :photos do
+    resources :comments
   end
   get '/logout', to: 'index#logout'
   get '/', to: 'index#index'
