db18189a79677d3b29eafe4da1f0872f7a447546
Owen Adrian <akrivus@gmail.com>
2020-02-24 18:44:10 -0700

    Validating file types.

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,9 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   storage :fog
-
+  
+  def extension_white_list
+    %w( jpg jpeg jfif png webp gif )
+  end
   def default_url(*args)
     '/images/avatar.jpg'
   end
